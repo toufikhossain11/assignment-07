3a9a06997b5a7a375e7eed0db4cebc3bd32255f7
toufikhossain11 <toufikhossain546@gmail.com>
2026-04-23 17:08:09 +0600

    page conditional add

--- src/Comoments/PageLayout/StatsPage/StatsPage.jsx
+++ src/Comoments/PageLayout/StatsPage/StatsPage.jsx
@@ -4,8 +4,8 @@ import { TimeLineContext } from '../../../context/TimelineContext';
 
 const StatsPage = () => {
     const { timeLineData } = useContext(TimeLineContext);
-    const {textData}=useContext(TimeLineContext);
-    const {videoData}=useContext(TimeLineContext);
+    const { textData } = useContext(TimeLineContext);
+    const { videoData } = useContext(TimeLineContext);
     const data = [
         { name: 'Text', value: timeLineData.length, fill: '#7E35E1' },
         { name: 'Call', value: textData.length, fill: '#244D3F' },
@@ -13,37 +13,40 @@ const StatsPage = () => {
 
     ]
     return (
-        <div className="px-6 py-10">
+        <div>
+            {timeLineData.length === 0 && textData.length === 0 && videoData.length === 0 ? <div className='max-w-3xl h-60 mx-auto shadow border-gray-400 m-5 flex justify-center items-center'><p className="text-[#244D3F] font-bold text-2xl  ">No Stats Data Found</p></div> : <div className="px-6 py-10">
 
-            <h1 className="text-3xl  font-bold mb-6 max-w-4xl mx-auto ">Friendship Analytics</h1>
+                <h1 className="text-3xl  font-bold mb-6 max-w-4xl mx-auto ">Friendship Analytics</h1>
 
-            <div className="bg-base-100 p-8 rounded-lg shadow max-w-4xl mx-auto text-center">
+                <div className="bg-base-100 p-8 rounded-lg shadow max-w-4xl mx-auto text-center">
 
-                <p className="text-xl text-[#244D3F] mb-6 text-left">
-                    By Interaction Type
-                </p>
+                    <p className="text-xl text-[#244D3F] mb-6 text-left">
+                        By Interaction Type
+                    </p>
 
-                <div className='mx-auto'>
-                    <PieChart style={{ width: '100%', maxWidth: '250px', maxHeight: '70vh', aspectRatio: 1 ,margin:'auto'}} responsive>
-                        <Pie
-                            data={data}
-                            innerRadius="80%"
-                            outerRadius="100%"
-                            
-                            cornerRadius="5%"
-                            fill="#8884d8"
-                            paddingAngle={4}
-                            dataKey="value"
-                            isAnimationActive={true}
-                        />
-                    <Legend></Legend>
-                    <Tooltip/>
-                    </PieChart>
+                    <div className='mx-auto'>
+                        <PieChart style={{ width: '100%', maxWidth: '250px', maxHeight: '70vh', aspectRatio: 1, margin: 'auto' }} responsive>
+                            <Pie
+                                data={data}
+                                innerRadius="80%"
+                                outerRadius="100%"
+
+                                cornerRadius="5%"
+                                fill="#8884d8"
+                                paddingAngle={4}
+                                dataKey="value"
+                                isAnimationActive={true}
+                            />
+                            <Legend></Legend>
+                            <Tooltip />
+                        </PieChart>
+                    </div>
+
+
                 </div>
-
-
-            </div>
+            </div>}
         </div>
+
     );
 };
 
--- src/Comoments/PageLayout/TimelinePage/TimelinePage.jsx
+++ src/Comoments/PageLayout/TimelinePage/TimelinePage.jsx
@@ -4,91 +4,71 @@ import { TimeLineContext } from '../../../context/TimelineContext';
 const TimelinePage = () => {
     const { timeLineData } = useContext(TimeLineContext)
 
-    const {textData}=useContext(TimeLineContext);
-    const {videoData}=useContext(TimeLineContext);
-    console.log(videoData)
+    const { textData } = useContext(TimeLineContext);
+    const { videoData } = useContext(TimeLineContext);
 
     return (
-        <div className="px-6 py-10 max-w-3xl mx-auto">
+        <div>
+            {timeLineData.length === 0 && textData.length === 0 && videoData.length === 0 ? <div className='max-w-3xl h-60 mx-auto shadow border-gray-400 m-5 flex justify-center items-center'><p className="text-[#244D3F] font-bold text-2xl  ">No Timeline Data Found</p></div>
+                
+             : <div className="px-6 py-10 max-w-3xl mx-auto">
 
-            <h1 className="text-2xl font-bold mb-6">Timeline</h1>
+                <h1 className="text-2xl font-bold mb-6">Timeline</h1>
 
-            <div className="mb-6">
-                <select className="select select-bordered w-full max-w-xs">
-                    <option>Filter timeline</option>
-                    <option>All</option>
-                    <option>Meetup</option>
-                    <option>Call</option>
-                    <option>Text</option>
-                    <option>Video</option>
-                </select>
-            </div>
-            {
-                timeLineData.map((data, ind) => {
-                    return <div key={ind} className="space-b-4">
-                        <div className="flex items-center gap-4 bg-base-100 p-4 rounded-lg shadow m-3">
-                            <span className="text-xl">📞</span>
-                            <div>
-                                <p className="font-bold text-[#244D3F]">
-                                    Call <span className="text-gray-500">{data.name}</span>
-                                </p>
-                                <p className="text-sm text-gray-400">
-                                    {new Date().toLocaleDateString("en-US")}
-                                </p>
+                <div className="mb-6">
+                    <select className="select select-bordered w-full max-w-xs">
+                        <option>Filter timeline</option>
+                        <option>All</option>
+                        <option>Meetup</option>
+                        <option>Call</option>
+                        <option>Text</option>
+                        <option>Video</option>
+                    </select>
+                </div>
+                {
+                    timeLineData.map((data, ind) => {
+                        return <div key={ind} className="space-b-4">
+                            <div className="flex items-center gap-4 bg-base-100 p-4 rounded-lg shadow m-3">
+                                <span className="text-xl">📞</span>
+                                <div>
+                                    <p className="font-bold text-[#244D3F]">
+                                        Call <span className="text-gray-500">{data.name}</span>
+                                    </p>
+                                    <p className="text-sm text-gray-400">
+                                        {new Date().toLocaleDateString("en-US")}
+                                    </p>
+                                </div>
                             </div>
                         </div>
+                    })
+                }
+
+                {textData.map((data, ind) => {
+                    return <div key={ind} className="flex items-center gap-4 bg-base-100 p-4 rounded-lg shadow m-3">
+                        <span className="text-xl">💬</span>
+                        <div>
+                            <p className="font-bold text-[#244D3F]">
+                                Text <span className="text-gray-500">{data.name}</span>
+                            </p>
+                            <p className="text-sm text-gray-400">{new Date().toLocaleDateString("en-US")}</p>
+                        </div>
                     </div>
-                })
-            }
-
-            {textData.map((data,ind)=>{
-                return<div key={ind} className="flex items-center gap-4 bg-base-100 p-4 rounded-lg shadow m-3">
-                    <span className="text-xl">💬</span>
-                    <div>
-                        <p className="font-bold text-[#244D3F]">
-                            Text <span className="text-gray-500">{data.name}</span>
-                        </p>
-                        <p className="text-sm text-gray-400">{new Date().toLocaleDateString("en-US")}</p>
+                })}
+                {videoData.map((data, ind) => {
+                    return <div key={ind} className="flex items-center gap-4 bg-base-100 p-4 rounded-lg shadow m-3">
+                        <span className="text-xl">🎥</span>
+                        <div>
+                            <p className="font-bold text-[#244D3F]">
+                                Video <span className="text-gray-500">{data.name}</span>
+                            </p>
+                            <p className="text-sm text-gray-400">{new Date().toLocaleDateString("en-US")}</p>
+                        </div>
                     </div>
-                </div>
-            }) }
-            {videoData.map((data,ind)=>{
-                return<div key={ind} className="flex items-center gap-4 bg-base-100 p-4 rounded-lg shadow m-3">
-                    <span className="text-xl">🎥</span>
-                    <div>
-                        <p className="font-bold text-[#244D3F]">
-                            Video <span className="text-gray-500">{data.name}</span>
-                        </p>
-                        <p className="text-sm text-gray-400">{new Date().toLocaleDateString("en-US")}</p>
-                    </div>
-                </div>
-            }) }
+                })}
 
-
-            {/* <div className="flex items-center gap-4 bg-base-100 p-4 rounded-lg shadow">
-                    <span className="text-xl">🤝</span>
-                    <div>
-                        <p className="font-bold text-[#244D3F]">
-                            Meetup <span className="text-gray-500 font-medium">with Tom Baker</span>
-                        </p>
-                        <p className="text-sm text-gray-400">March 29, 2026</p>
-                    </div>
-                </div> */}
-
-            
-
-            {/* 
-                <div className="flex items-center gap-4 bg-base-100 p-4 rounded-lg shadow">
-                    <span className="text-xl">🎥</span>
-                    <div>
-                        <p className="font-medium">
-                            Video <span className="text-gray-500">with Aisha Patel</span>
-                        </p>
-                        <p className="text-sm text-gray-400">March 23, 2026</p>
-                    </div>
-                </div> */}
-
+            </div>}
         </div>
+
     );
 };
 
